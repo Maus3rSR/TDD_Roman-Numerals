aa32f2db4674869a68f216c9b58f0099b554498e
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-05-08 16:12:36 +0200

    green

--- src/2025_05_08_RomanNumeral.test.ts
+++ src/2025_05_08_RomanNumeral.test.ts
@@ -6,6 +6,9 @@ function romanianToDecimal(romanian: string): number {
     V: 5,
     X: 10,
     L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
   };
   let finalNumber = 0;
   let index = 0;
@@ -37,6 +40,9 @@ test.each([
   ['X', 10],
   ['IX', 9],
   ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000],
 ])('%s should give %s', (input, expected) => {
   expect(romanianToDecimal(input)).toBe(expected);
 });
