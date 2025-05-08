a1e4c15e2524c7ee0d7be5158043d9a5b7ae087b
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-05-08 15:24:14 +0200

    refactor

--- src/2025_05_08_RomanNumeral.test.ts
+++ src/2025_05_08_RomanNumeral.test.ts
@@ -3,15 +3,15 @@ import { test, expect } from 'vitest';
 function romanianToDecimal(romanian: string): number {
   let finalNumber = 0;
 
-  if (romanian === 'I') {
+  if (romanian[0] === 'I') {
     finalNumber = 1;
   }
 
-  if (romanian === 'II') {
+  if (romanian[1] === 'I') {
     finalNumber = 2;
   }
 
-  if (romanian === 'III') {
+  if (romanian[2] === 'I') {
     finalNumber = 3;
   }
 
