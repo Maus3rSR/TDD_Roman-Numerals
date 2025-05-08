261e5e49a4cd817a668d0d0652a9928492a30b55
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-05-08 15:42:33 +0200

    green

--- src/2025_05_08_RomanNumeral.test.ts
+++ src/2025_05_08_RomanNumeral.test.ts
@@ -2,13 +2,21 @@ import { test, expect } from 'vitest';
 
 function romanianToDecimal(romanian: string): number {
   let finalNumber = 0;
+  let previousLetter: string = '';
+  let index = 0;
 
-  for (let c of romanian) {
-    if (c === 'V') {
+  while (romanian[index]) {
+    const c = romanian[index];
+    if (c === 'V' && previousLetter === 'I') {
+      finalNumber += 3;
+    } else if (c === 'V') {
       finalNumber += 5;
     } else {
       finalNumber += 1;
     }
+
+    previousLetter = c;
+    index++;
   }
 
   return finalNumber;
@@ -19,6 +27,7 @@ test.each([
   ['II', 2],
   ['III', 3],
   ['V', 5],
+  ['IV', 4],
 ])('%s should give %s', (input, expected) => {
   expect(romanianToDecimal(input)).toBe(expected);
 });
